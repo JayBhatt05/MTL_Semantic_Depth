83919ab0af0cdfb67fea1641825ca5acb4b65c26
JayBhatt05 <139049460+JayBhatt05@users.noreply.github.com>
2025-02-25 19:02:05 +0530

    Create AutomaticWeightedLoss.py

---
+++ MTL/AutomaticWeightedLoss.py
@@ -0,0 +1,16 @@
+import torch
+
+class AutomaticWeightedLoss(nn.Module):
+    '''
+    Taken from - https://github.com/Mikoto10032/AutomaticWeightedLoss/tree/master
+    '''
+    def __init__(self, num=2):
+        super(AutomaticWeightedLoss, self).__init__()
+        params = torch.ones(num, requires_grad=True)
+        self.params = torch.nn.Parameter(params)
+
+    def forward(self, *x):
+        loss_sum = 0
+        for i, loss in enumerate(x):
+            loss_sum += 0.5 / (self.params[i] ** 2) * loss + torch.log(1 + self.params[i] ** 2)
+        return loss_sum
